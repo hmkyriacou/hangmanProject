e08d5a3ac53fd9e13ea3d40c3f3a5ddaa140735d
Harrison Kyriacou <hmkyriacou@Harrisons-MacBook-Pro.local>
2018-01-03 09:28:24 -0500

    HangMan commit

--- blank_Maker.py
+++ blank_Maker.py
@@ -1,11 +1,29 @@
-def dashMaker(word):
+def dashMaker(word,letter):
   blanks = ""
+  c = 0
   for blankMaker in range(len(word)):
-    blanks += "-"
-  return blanks
-  
-  
-#    ****MAIN***
-x = "HELLO"
-print(dashMaker(x))
+      for letterCounter in range(len(word)):
+          wordPs = input1.find(letter)
+          if wordPs == c:
+              blanks+= letter
+          else:
+              blanks += "-"
 
+      c+=1
+
+  return blanks
+
+def letterChanger(input1, letter):
+    wordPs = input1.find(letter)
+    rWordPs = input1.rfind(letter)
+    wordPsOut = str(wordPs) + str(rWordPs)
+    while (wordPs != rWordPs):
+        c = wordPs + 1
+        wordPs = input1.find(letter, c)
+    return wordPsOut
+
+
+#    ****MAIN***
+input1 = "HELOLLO"
+print(letterChanger(input1, "L"))
+#print(dashMaker(input1,"L",letterChanger(input1, "L")))
