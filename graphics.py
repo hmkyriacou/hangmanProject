39fb8ef954affa77d1c38b8607e1a2693153927f
griffinthurber <34350501+griffinthurber@users.noreply.github.com>
2018-01-08 13:44:09 -0500

    graphics2.py
    Need the function for the graphic

--- graphics.py
+++ graphics.py
@@ -1,4 +1,70 @@
-print("hello")
+print ("________      ")
+print ("|      |      ")
+print ("|             ")
+print ("|             ")
+print ("|             ")
+print ("|             ")
+
+
+def wordCheck(word):
+  word = input("Enter your word: ")
+  word = word.upper()
+  
+  length = len(word)
+  if (len(word)> 10 or len(word) < 3):
+    print("Please input a word with 3 to 10 letters.")
+    wordCheck(word)
+  else:
+    return(word)
+    
+def guessCheck(guess):
+  guess = input("Guess a letter: ")
+  if (len(guess) > 1):
+    print("Please only input one letter.")
+    guess = input("Guess a letter: ")
+    guessCheck(guess)
+  else:
+    return guess
+  
+begin = input("Would you like your word to be input or random? ")
+begin = begin.upper()
+word = " "
+
+
+if (begin == "RANDOM"):
+  #Gets the number of words in Word.txt
+  infile = open("Word.txt", "r")
+  c = 0
+  line = infile.readline()
+  while line:
+    c += 1
+    line = infile.readline()
+  infile.close() 
+
+  #Gets a random word from Word.txt 
+  import random
+  wordn = random.randint(1, c)
+  infile1 = open("Word.txt", "r")
+  for x in range(wordn):
+    word = infile1.readline()
+  word = word.replace("\n", "")
+  infile1.close()
+  word = word.upper()
+  
+
+  
+elif (begin == "INPUT"):
+  wordCheck(word)
+
+
+guess = ""
+guessCheck(guess)
+print("Guess: " + guess)
+
+
+
+
+
 def hangmanGraphics(guesses):
         if (guesses == 0):
             print ("________      ")
@@ -79,65 +145,9 @@ def hangmanGraphics(guesses):
             print ("|             ")
             print ("Game Over")
             
-def wordCheck(word):
-  word = input("Enter your word: ")
-  word = word.upper()
-  
-  length = len(word)
-  if (len(word)> 10 or len(word) < 3):
-    print("Please input a word with 3 to 10 letters.")
-    wordCheck(word)
-  else:
-    return(word)
-    
-def guessCheck(guess):
-  guess = input("Guess a letter: ")
-  if (len(guess) > 1):
-    print("Please only input one letter.")
-    guess = input("Guess a letter: ")
-    guessCheck(guess)
-  else:
-    return guess
-  
-begin = input("Would you like your word to be input or random? ")
-begin = begin.upper()
-word = " "
-
-
-if (begin == "RANDOM"):
-  #Gets the number of words in Word.txt
-  infile = open("Word.txt", "r")
-  c = 0
-  line = infile.readline()
-  while line:
-    c += 1
-    line = infile.readline()
-  infile.close() 
-
-  #Gets a random word from Word.txt 
-  import random
-  wordn = random.randint(1, c)
-  infile1 = open("Word.txt", "r")
-  for x in range(wordn):
-    word = infile1.readline()
-  word = word.replace("\n", "")
-  infile1.close()
-  word = word.upper()
-  
 
   
-elif (begin == "INPUT"):
-  wordCheck(word)
-  
-
-print ("________      ")
-print ("|      |      ")
-print ("|             ")
-print ("|             ")
-print ("|             ")
-print ("|             ")
 
 
-guess = ""
-guessCheck(guess)
-print("Guess: " + guess)
+
+
