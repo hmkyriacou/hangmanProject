c8a4fcd79229e2a593ae7f5b39da5dc13b961c0b
hmkyriacou <hmkyriacou14@gmail.com>
2018-01-26 13:26:12 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+def guessChecker(word,letter,previous="---------------"):
+  blanks = ""
 
+  c = 0
+  for letterCounter in range(len(word)):
+      if word[letterCounter] == letter and previous[letterCounter] == "-" :
+          blanks += letter
+      elif previous[letterCounter] == "-":
+          blanks += "-"
+      else:
+          blanks += previous[letterCounter]
+
+  return blanks
